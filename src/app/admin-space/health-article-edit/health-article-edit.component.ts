addb2a656e17bf25bbac3a9972fde6379d3062a0
Raphael <elieraphm@gmail.com>
2017-10-17 22:23:59 +0300

    add an admin space with a health-article edit

--- src/app/admin-space/health-article-edit/health-article-edit.component.ts
+++ src/app/admin-space/health-article-edit/health-article-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { HealthArticleService } from '../../services/health-article/health-article-service';
+import { HealthArticle } from '../../models/objects-models/health-article';
+import { SuccessService } from '../../notif-to-user/success/success.service';
+import { ErrorService } from '../../notif-to-user/errors/error.service';
 
 @Component({
   selector: 'app-health-article-edit',
@@ -7,9 +12,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HealthArticleEditComponent implements OnInit {
 
-  constructor() { }
+  rForm: FormGroup;
+  loaded: boolean;
+  title: string;
+  imageBase64: string;
+  imageBase64Temp: string;
+
+  content: string
+
+
+  constructor(private fb: FormBuilder,
+    private healthArticleService: HealthArticleService,
+    private successService: SuccessService,
+    private errorService: ErrorService) {
+    this.loaded = false;
+    this.rForm = fb.group({
+      'title': [null, Validators.required],
+      'content': [null, Validators.required]
+    });
+  }
+
+  addPost(post) {
+    this.title = post.title;
+    this.content = post.content;
+    this.imageBase64 = this.imageBase64Temp;
+
+    //create
+    const article = new HealthArticle('null', this.title, this.imageBase64, this.content);
+    //reset form
+    this.rForm.reset();
+    this.loaded = false;
+    this.imageBase64Temp = '';
+    this.title = '';
+    this.content = '';
+    //send request
+    this.healthArticleService.addArticle(article)
+      .subscribe(
+      data => {
+        this.successService.handleSuccess(data.json());
+      },
+      error => {
+        this.errorService.handleError(error);
+      }
+      );
+
+  }
+
+  onUploadFile(files) {
+    var myReader: FileReader = new FileReader();
+    myReader.onloadend = (e) => {
+      this.imageBase64Temp = myReader.result;
+      this.loaded = true;
+    }
+
+    myReader.readAsDataURL(files[0])
+  }
 
   ngOnInit() {
+
   }
 
 }
