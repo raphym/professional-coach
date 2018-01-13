8957727a80655cc0766fff3c0410c9e5bc9f2123
Raphael <elieraphm@gmail.com>
2018-01-13 23:34:58 +0200

    update sty articles prev in the home page

--- src/app/articles/article.component.ts
+++ src/app/articles/article.component.ts
@@ -4,6 +4,7 @@ import { SuccessService } from '../shared/components/notif-to-user/success/succe
 import { ErrorService } from '../shared/components/notif-to-user/errors/error.service';
 import { LoaderService } from '../shared/components/loader/loader.service';
 import { Article } from '../shared/models/objects-models/article';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-articles',
@@ -17,20 +18,25 @@ export class ArticlesComponent implements OnInit {
     private articleService: ArticleService,
     private successService: SuccessService,
     private errorService: ErrorService,
-    private loaderService: LoaderService, ) {
+    private loaderService: LoaderService,
+    private router: Router) {
 
   }
   ngOnInit() {
     this.articleService.getLastNewArticle().subscribe(
       data => {
-        console.log(data);
-        this.lastArticle = new Article(data.id, data.title, data.image, data.content, data.intro, data.date);
+        this.lastArticle = new Article(data._id, data.title, data.image, data.content, data.intro, data.date);
       },
       error => {
         console.log('error');
         console.log(error);
       }
     );
+  }
+  onClick() {
+    var path = 'view-article/';
+    path += this.lastArticle._id;
+    this.router.navigateByUrl(path);
   }
 
 }
