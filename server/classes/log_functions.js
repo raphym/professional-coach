e3255d6e1920ca465535c5aeff7ffdcfcdfa435c
raphym <elieraphm@gmail.com>
2018-04-10 23:34:08 +0300

    added script to manage logs

--- server/classes/log_functions.js
+++ server/classes/log_functions.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var config_log = require('../../config/log_db');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var pdf = require('html-pdf');
 
 const config_log_uri = config_log.api + config_log.user + ':' + config_log.pswd + config_log.server + ':' + config_log.port + '/' + config_log.name;
 var conn = mongoose.createConnection(config_log_uri, function () {
@@ -17,6 +18,150 @@ var Logs = conn.model('log', new Schema({
     date: { type: Date, required: true }
 }));
 
+//script
+logScript = function () {
+    console.log('logScript start');
+    manageGeneralLog().then(function (responseManageGeneralLog) {
+        manageErrorLog().then(function (responseManageErrorLog) {
+            console.log('logScript end');
+        }).catch(function (errorManageErrorLog) {
+            console.log('logScript end with error');
+            console.log(error);
+        })
+    }).catch(function (errorManageGeneralLog) {
+        console.log(error);
+        //try to manage error logs even if manage general logs failed
+        manageErrorLog().then(function (responseManageErrorLog) {
+            console.log('logScript end with error');
+        }).catch(function (errorManageErrorLog) {
+            console.log('logScript end with error');
+            console.log(error);
+        })
+    })
+}
+
+//manage general log
+manageGeneralLog = function () {
+    return new Promise(function (resolve, reject) {
+        Logs.find({ type: 'GENERAL' }, function (err, gLogs) {
+            if (err) {
+                console.log('error to find gLogs');
+                reject({ result: 'FAILED', message: 'error to find general logs' });
+            }
+            else {
+                if (gLogs == undefined || gLogs == null || gLogs.length == 0)
+                    reject({ result: 'FAILED', message: 'error: gLogs == undefined || gLogs == null || gLogs.length == 0' });
+                else {
+                    var generalHtml = "<!DOCTYPE html><html><head><title>G-Fit Log</title></head><body>";
+                    generalHtml += "<h1 style='text-align:center;'>G-Fit Log</h1>";
+                    generalHtml += "<h1 style='text-align:center;'>GENERAL</h1>";
+                    generalHtml += "<div>Number of General logs: ";
+                    generalHtml += gLogs.length;
+                    generalHtml += "</div><br>";
+                    for (var i = 0; i < gLogs.length; i++) {
+                        generalHtml += generateSubLogHtml(i, gLogs[i].type, gLogs[i].title, gLogs[i].date, gLogs[i].itemId, gLogs[i].userIp, gLogs[i].content);
+                        generalHtml += '<hr>';
+                    }
+                    generalHtml += "</body></html>";
+
+                    pdfLog(generalHtml, 'generalLogs').then(function (response) {
+                        if (response.result == 'SUCCESS')
+                            resolve({ result: 'SUCCESS', data: generalHtml });
+                    }).catch(function (error) {
+                        reject({ result: 'FAILED', message: 'error to generate Pdf' });
+                    })
+                }
+            }
+        });
+    });
+}
+
+//manage error log
+manageErrorLog = function () {
+    return new Promise(function (resolve, reject) {
+        Logs.find({ type: 'ERROR' }, function (err, eLogs) {
+            if (err) {
+                console.log('error to find gLogs');
+                reject({ result: 'FAILED', message: 'error to find error logs' });
+            }
+            else {
+                if (eLogs == undefined || eLogs == null || eLogs.length == 0)
+                    reject({ result: 'FAILED', message: 'error: eLogs == undefined || eLogs == null || eLogs.length == 0' });
+                else {
+                    var errorHtml = "<!DOCTYPE html><html><head><title>G-Fit Log</title></head><body>";
+                    errorHtml += "<h1 style='text-align:center;'>G-Fit Log</h1>";
+                    errorHtml += "<h1 style='text-align:center;'>ERROR</h1>";
+                    errorHtml += "<div>Number of Error logs: ";
+                    errorHtml += eLogs.length;
+                    errorHtml += "</div><br>";
+                    for (var i = 0; i < eLogs.length; i++) {
+                        errorHtml += generateSubLogHtml(i, eLogs[i].type, eLogs[i].title, eLogs[i].date, eLogs[i].itemId, eLogs[i].userIp, eLogs[i].content);
+                        errorHtml += '<hr>';
+                    }
+                    errorHtml += "</body></html>";
+
+                    pdfLog(errorHtml, 'errorLogs').then(function (response) {
+                        if (response.result == 'SUCCESS')
+                            resolve({ result: 'SUCCESS', data: errorHtml });
+                    }).catch(function (error) {
+                        reject({ result: 'FAILED', message: 'error to generate Pdf' });
+                    })
+                }
+            }
+        });
+    });
+}
+
+//create pdf
+pdfLog = function (html, type) {
+    return new Promise(function (resolve, reject) {
+        pdf.create(html).toBuffer(function (err, buffer) {
+            fs.writeFile(type + '.pdf', buffer, function (err) {
+                if (err) {
+                    reject({ result: 'FAILED' });
+                }
+                else {
+                    resolve({ result: 'SUCCESS' });
+                }
+            });
+        });
+    });
+}
+
+//generate sub log html for each event
+generateSubLogHtml = function (i, type, title, date, itemId, userIp, content) {
+    var alert_string = 'security_$_';
+    var html = "";
+    html += "<div><span style='font-weight: bold;'>Num: </span>";
+    html += (i + 1);
+    html += "</div>";
+    if (title.indexOf(alert_string) > -1) {
+        html += "<div><span style='font-weight: bold;color:red;'>Title: </span>";
+    }
+    else {
+        html += "<div><span style='font-weight: bold;'>Title: </span>";
+    }
+    html += title;
+    html += "</div>";
+    html += "<div><span style='font-weight: bold;'>Type: </span>";
+    html += type;
+    html += "</div>";
+    html += "<div><span style='font-weight: bold;'>Date: </span>";
+    html += date;
+    html += "</div>";
+    html += "<div><span style='font-weight: bold;'>Item Id: </span>";
+    html += itemId;
+    html += "</div>";
+    html += "<div><span style='font-weight: bold;'>User Ip: </span>";
+    html += userIp;
+    html += "</div>";
+    html += "<div><span style='font-weight: bold;'>Content: </span>";
+    html += content;
+    html += "</div>";
+    return html;
+}
+
+var timeo = setTimeout(logScript, 6000);
 module.exports = class LogFunctions {
     constructor() {
     }
